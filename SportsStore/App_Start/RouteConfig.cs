acafb65091f70ee28efa33cf0272207d491d5628
Aleksandr Smalyokha <aleksandr_kesha@mail.ru>
2017-03-04 12:12:41 +0700

    latest changes

--- SportsStore/App_Start/RouteConfig.cs
+++ SportsStore/App_Start/RouteConfig.cs
@@ -12,6 +12,9 @@ namespace SportsStore
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
             routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
             routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
+
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
+            routes.MapPageRoute("admin_products", "admin/products", "~/Pages/Admin/Products.aspx");
         }
     }
 }
